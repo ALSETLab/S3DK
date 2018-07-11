20731d04b243384b190bd1f18632ca439d5b598d
Grey-J <38929027+Grey-J@users.noreply.github.com>
2018-07-11 14:57:43 -0400

    Configuration for site

--- Documentation/Website/website/siteConfig.js
+++ Documentation/Website/website/siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: 'Documentation' /* title for your website */,
   tagline: 'S3DK - STRONGgrid',
   url: 'https://ALSETLab.github.io' /* your website url */,
-  baseUrl: '/S3DK_documentation/' /* base url for your project */,
+  baseUrl: '/S3DK-documentation/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'S3DK_documentation',
+  projectName: 'S3DK-documentation',
   organizationName: 'ALSETLab',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
