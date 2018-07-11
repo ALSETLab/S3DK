1eaedfdb5b873d5e61cc679df4b0add58d274cf2
Grey-J <38929027+Grey-J@users.noreply.github.com>
2018-07-11 13:44:05 -0400

    Configuration for site

--- Documentation/Website files/website/siteConfig.js
+++ Documentation/Website files/website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'Documentation' /* title for your website */,
   tagline: 'S3DK - STRONGgrid',
-  url: 'https://your-docusaurus-test-site.com' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  url: 'https://ALSETLab.github.io' /* your website url */,
+  baseUrl: '/S3DK/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
