cf86a9c507ed650f62f8b0c8a8c0c63f70638fab
Grey-J <38929027+Grey-J@users.noreply.github.com>
2018-07-24 10:48:47 -0400

    Website fix

--- Documentation/Website/website/siteConfig.js
+++ Documentation/Website/website/siteConfig.js
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'S3DK',
-  organizationName: 'ALSETLabs',
+  organizationName: 'ALSETLab',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
